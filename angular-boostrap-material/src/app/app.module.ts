6f9cd365d33c9daf67c45834ae0cc06c08bd9d19
Manjula Jayawardana <manjulajayawardana@gmail.com>
2019-07-03 21:04:52 +0530

    angular bootstrap create basic components.

--- angular-boostrap-material/src/app/app.module.ts
+++ angular-boostrap-material/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './core/app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
